727a9a64de1efc5c9fb2f1668e52068ee420d73e
ashsih naik <ashishnaik4444@gmail.com>
2022-02-11 20:41:47 +0530

    Html reports

--- PageObjects/Practise_form.py
+++ PageObjects/Practise_form.py
@@ -1,3 +1,6 @@
+import time
+
+from selenium.webdriver import Keys, ActionChains
 from selenium.webdriver.common.by import By
 
 
@@ -5,11 +8,77 @@ class Practise_form_po:
 
     Forms_click_expand = (By.XPATH,"//div[@class='accordion']/div[2]/span/div/div[2]")
     practise_form_click = (By.XPATH,"//span[text()='Practice Form']")
+    enter_first_name = (By.CSS_SELECTOR,"#firstName")
+    enter_last_name = (By.CSS_SELECTOR, "#lastName")
+    radio_button_gender_female = (By.XPATH, "//label[@for='gender-radio-2']")
+    enter_user_number = (By.CSS_SELECTOR, "#userNumber")
+    enter_email = (By.CSS_SELECTOR, "#userEmail")
+    hobbies_checkbox_reading = (By.XPATH, "//label[@for='hobbies-checkbox-2']")
+    subjects_form_Maths = (By.CSS_SELECTOR, "#subjectsContainer")
+    upload_picture = (By.CSS_SELECTOR, "#uploadPicture")
+    current_address = (By.CSS_SELECTOR, "#currentAddress")
+    select_state = (By.CSS_SELECTOR, "#state")
+    select_city = (By.CSS_SELECTOR, "#city")
+    submit_click = (By.CSS_SELECTOR, "#submit")
+    close_submit_form = (By.CSS_SELECTOR, "#closeLargeModal")
+    date_picker = (By.CSS_SELECTOR, "#dateOfBirthInput")
+    date_picker_month = (By.CSS_SELECTOR, ".react-datepicker__month-select")
+    date_picker_year = (By.CSS_SELECTOR, ".react-datepicker__year-select")
+    student_name_entered = (By.XPATH, "//td[text() = 'Student Name']/following-sibling::td")
 
     def __init__(self,driver):
         self.driver = driver
 
     def click_practise_form(self):
         self.driver.find_element(*Practise_form_po.practise_form_click).click()
+
+    def click_first_name(self):
+        return self.driver.find_element(*Practise_form_po.enter_first_name)
+
+    def click_last_name(self):
+        return self.driver.find_element(*Practise_form_po.enter_last_name)
+
+    def click_gender_radio_female(self):
+        self.driver.find_element(*Practise_form_po.radio_button_gender_female).click()
+
+    def click_user_number(self):
+        return self.driver.find_element(*Practise_form_po.enter_user_number)
+
+    def click_email_id(self):
+        return self.driver.find_element(*Practise_form_po.enter_email)
+
+    def click_subject_form_Maths(self):
+        self.driver.find_element(*Practise_form_po.subjects_form_Maths).click()
+        actions = ActionChains(self.driver)
+        actions.send_keys("Maths").perform()
+        time.sleep(2)
+        actions.send_keys(Keys.ENTER).perform()
+
+    def click_current_address(self):
+        return self.driver.find_element(*Practise_form_po.current_address)
+
+    def click_hobbies_checkbox_reading(self):
+        self.driver.find_element(*Practise_form_po.hobbies_checkbox_reading).click()
+
+    def click_upload_image(self):
+        return self.driver.find_element(*Practise_form_po.upload_picture)
+
+    def click_state(self):
+        return self.driver.find_element(*Practise_form_po.select_state).click()
+
+    def click_city(self):
+        return self.driver.find_element(*Practise_form_po.select_city).click()
+
+    def click_on_submit(self):
+        return self.driver.find_element(*Practise_form_po.submit_click).click()
+
+    def click_close_submitform(self):
+        return self.driver.find_element(*Practise_form_po.close_submit_form).click()
+
+    def date_picker_click(self):
+        return self.driver.find_element(*Practise_form_po.date_picker)
+
+    def get_student_name(self):
+        return self.driver.find_element(*Practise_form_po.student_name_entered).text
 
 
--- Tests/Test_Forms_Page.py
+++ Tests/Test_Forms_Page.py
@@ -1,15 +1,49 @@
+import os
 from PageObjects.Homepage import HomePageCls
 from PageObjects.Practise_form import Practise_form_po
 from Utilities.BaseClass import Baseclass
+from config import PROJECT_PATH
 
 
 class Test_forms(Baseclass):
 
     def test_practise_form(self):
         Fp = HomePageCls(self.driver)
-        Fp.Forms_homepage()
-        # Fp1 = Practise_form_po(self.driver)
-        # Fp1.click_practise_form()
+        Fp.Forms_homepage().click()
+        Fp1 = Practise_form_po(self.driver)
+        Fp1.click_practise_form()
+        Fp1.click_first_name().send_keys("Ashish")
+        Fp1.click_last_name().send_keys("Naik")
+        Fp1.click_email_id().send_keys("ashishnaik@zmail.com")
+        Fp1.click_gender_radio_female()
+        Fp1.click_user_number().send_keys("8127272712")
+        Fp1.click_current_address().send_keys("Test Address %$!@@!^%@ Line 1")
+        Fp1.click_subject_form_Maths()
+        Fp1.click_hobbies_checkbox_reading()
+        Fp1.click_upload_image().send_keys(os.path.join(PROJECT_PATH,"Datas\\pexels-pixabay-65894.jpg"))
+        self.Explicit_wait_by_clickable(Fp1.select_state)
+        self.scroll_vertical(800)
+        Fp1.click_state()
+        self.dropdown_byvalue("Uttar Pradesh")
+        Fp1.click_city()
+        self.dropdown_byvalue("Agra")
+        self.date_picker_by_value(1, 1, 1995, Fp1.date_picker_month, Fp1.date_picker_year)
+        self.driver.get_screenshot_as_file(".\\Screenshots\\Forms_entries.png")
+        Fp1.click_on_submit()
+        student_name = Fp1.get_student_name()
+        assert student_name == "Ashish Naik"
+        self.driver.get_screenshot_as_file(".\\Screenshots\\Forms_entries_submit.png")
+        Fp1.click_close_submitform()
+
+
+
+
+
+
+
+
+
+
 
 
 
